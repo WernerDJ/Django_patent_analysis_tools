9b0187140ac4a8b4ad0c9be09ffd2d7ac33c0354
WernerDJ <wernerdjuan@gmail.com>
2025-04-08 16:08:39 +0200

    analpat

--- analpat/asgi.py
+++ analpat/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Analpat project.
+ASGI config for analpat project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Analpat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analpat.settings")
 
 application = get_asgi_application()
